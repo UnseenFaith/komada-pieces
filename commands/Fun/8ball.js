c031706ad1c001f4e18643bfdd341d30f5eff019
York <aarghgames@gmail.com>
2016-10-20 16:20:16 +0100

    Reduced response array to one line, fixed spacing

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -1,29 +1,4 @@
-var answers = [
-  "Maybe.",
-  "Certainly not.",
-  "I hope so.",
-  "Not in your wildest dreams.",
-  "There is a good chance.",
-  "Quite likely.",
-  "I think so.",
-  "I hope not.",
-  "I hope so.",
-  "Never!",
-  "Fuhgeddaboudit.",
-  "Ahaha! Really?!?",
-  "Pfft.",
-  "Sorry, bucko.",
-  "Hell, yes.",
-  "Hell to the no.",
-  "The future is bleak.",
-  "The future is uncertain.",
-  "I would rather not say.",
-  "Who cares?",
-  "Possibly.",
-  "Never, ever, ever.",
-  "There is a small chance.",
-  "Yes!"
-];
+var answers = ["Maybe.", "Certainly not.", "I hope so.", "Not in your wildest dreams.", "There is a good chance.", "Quite likely.", "I think so.", "I hope not.", "I hope so.", "Never!", "Fuhgeddaboudit.", "Ahaha! Really?!?", "Pfft.", "Sorry, bucko.", "Hell, yes.", "Hell to the no.", "The future is bleak.", "The future is uncertain.", "I would rather not say.", "Who cares?", "Possibly.", "Never, ever, ever.", "There is a small chance.", "Yes!"];
 
 exports.run = (client, msg) => {
   if (msg.content.endsWith("?")) {
@@ -36,7 +11,7 @@ exports.run = (client, msg) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["8","magic","8ball"],
+  aliases: ["8", "magic", "8ball"],
   permLevel: 0,
   botPerms: [],
   requiredFuncs: []
