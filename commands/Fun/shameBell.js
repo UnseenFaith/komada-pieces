8f3169ce7d7eef85a77d8de97d7864c411cbd1b7
York <aarghgames@gmail.com>
2016-10-20 22:50:43 +0100

    Added more commands
    Added shameBell and presence commands

---
+++ commands/Fun/shameBell.js
@@ -0,0 +1,19 @@
+exports.run = (client, msg, [user]) => {
+  msg.channel.sendMessage(`:bell: SHAME :bell: ${user} :bell: SHAME :bell:`).catch(error => console.log(error.stack));
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+  botPerms: [],
+  requiredFuncs: []
+};
+
+exports.help = {
+  name: "shame",
+  description: "Rings a bell on the server shaming the mentioned person",
+  usage: "<user:user>",
+  usageDelim: ""
+};
