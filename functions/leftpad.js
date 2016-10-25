86e73a639e5964f8c4c18017f6a665d64b536721
Cat <kaubariskis123@gmail.com>
2016-10-25 22:51:19 +0300

    lets merge this to one file

--- functions/leftpad.js
+++
@@ -1,7 +0,0 @@
-module.exports = (text, number, char = '0') => {
-    if (typeof(text) === "string") {
-        return String(text).length >= number ? '' + text : (String(char).repeat(number) + text).slice(-number);
-    } else {
-        return text;
-    }
-};
