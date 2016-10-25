eb6a51bf541344c0aba2ac9e935918e560ae81c5
York <aarghgames@gmail.com>
2016-10-25 17:09:02 +0100

    Updated Urban
    Added results filter, you can now view different definitions!

--- commands/Fun/urban.js
+++ commands/Fun/urban.js
@@ -1,31 +1,36 @@
 let request = require('request');
-exports.run = (client, msg, [search, resultnum]) => {
-  var baseUrl = 'http://api.urbandictionary.com/v0/define?term=';
-  var theUrl = baseUrl + search;
-  request({
-    url: theUrl,
-    json: true
-  }, function(error, response, body) {
-    if (!resultnum) resultnum = 0;
-    let result = body.list[resultnum];
-    if (result) {
-      var definition = [
-        `**Word:** ${search}`,
-        '',
-        `**Definition:**\n_${result.definition}_`,
-        '',
-        `**Example:**\n${result.example}`,
-        `<${result.permalink}>`
-      ];
-      msg.channel.sendMessage(definition).catch(error => console.log(error.stack));
-    } else {
-      msg.channel.sendMessage('No entry found.').catch(error => console.log(error.stack));
-    }
-  });
+exports.run = (client, msg, [search, resultNum]) => {
+    var baseUrl = 'http://api.urbandictionary.com/v0/define?term=';
+    var theUrl = baseUrl + search;
+    request({
+        url: theUrl,
+        json: true
+    }, function(error, response, body) {
+        if (!resultNum) {
+            resultNum = 0;
+        } else if (resultNum > 1) {
+            resultNum -= 1;
+        }
+        let result = body.list[resultNum];
+        if (result) {
+            var definition = [
+                `**Word:** ${search}`,
+                '',
+                `**Definition:** ${resultNum += 1} out of ${body.list.length}\n_${result.definition}_`,
+                '',
+                `**Example:**\n${result.example}`,
+                `<${result.permalink}>`
+            ];
+            msg.channel.sendMessage(definition).catch(error => console.log(error.stack));
+        } else {
+            msg.channel.sendMessage('No entry found.').catch(error => console.log(error.stack));
+        }
+    });
 };
 
 exports.conf = {
     enabled: true,
+		selfbot: false,
     guildOnly: false,
     aliases: [],
     permLevel: 2,
@@ -37,5 +42,5 @@ exports.help = {
     name: "urban",
     description: "Searches the Urban Dictionary library for a definition to the search term.",
     usage: "<search:str>",
-    usageDelim: " "
+    usageDelim: ", "
 };
