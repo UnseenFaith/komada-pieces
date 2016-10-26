190bac77128cd3b75bd41c9976b8ab88f93038a6
Johnson Chen <CyberiumShadow@users.noreply.github.com>
2016-10-27 06:59:17 +1100

    Optimised Code by York
    Thanks tO York for optimisImg the code.

--- commands/Self/nick.js
+++ commands/Self/nick.js
@@ -1,28 +1,24 @@
 exports.run = (client, msg, [nick]) => {
-  if (!nick) {
-    msg.member.setNickname('');
-    msg.edit('Nickname has been cleared');
-    msg.delete(10000);
-  } else {
-    msg.member.setNickname(`${nick}`);
-    msg.edit(`Nickname has been changed to ${nick}`);
-    msg.delete(10000);
-  }
+    var newNick = !nick ? "" : nick.split(',').join(' ');
+    msg.guild.member(client.user).setNickname(newNick).then(() => {
+        let text = newNick ? 'Nickname changed to ' + newNick : 'Nickname Cleared';
+        msg.channel.sendMessage(text).then(response => response.delete(10000)).catch(error => console.log(error.stack));
+    }).catch(error => console.log(error.stack));
 };
 
 exports.conf = {
-  enabled: true,
-  selfbot: false,
-  guildOnly: true,
-  aliases: [],
-  permLevel: 3,
-  botPerms: ["CHANGE_NICKNAME"],
-  requiredFuncs: []
+    enabled: true,
+    selfbot: false,
+    guildOnly: true,
+    aliases: [],
+    permLevel: 3,
+    botPerms: ["CHANGE_NICKNAME"],
+    requiredFuncs: []
 };
 
 exports.help = {
-  name: "nick",
-  description: "Set's the bot's nickname",
-  usage: "[nick:str]",
-  usageDelim: " "
+    name: "nick",
+    description: "Set's the bot's nickname",
+    usage: "[nick:str]",
+    usageDelim: ", "
 };
