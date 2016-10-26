c89ca11cc63cbb06f33ef7f84eea11086418e3aa
Faith <tba28012@hotmail.com>
2016-10-26 02:16:08 -0400

    blacklist.js command
    This command is used for blacklisting users from using commands. Used in conjunction with my blacklist inhibitor.

---
+++ commands/Moderation/blacklist.js
@@ -0,0 +1,41 @@
+const fs = require('fs-extra');
+
+/** Runs Blacklist Command
+  * @param {Object} client - The Discord.js client
+  * @param {Object} msg - The command message
+  * @param {(Object|String)} user - The mentioned user or user ID
+  */
+
+exports.run = (client, msg, [user]) => {
+  fs.readJSON('blacklist.json', (err, blacklist) => {
+    if (blacklist != undefined && blacklist.indexOf(user.id) !== -1) {
+      msg.reply("Geez... do you hate that person or something.. They're already blacklisted! :smile:")
+      return;
+    } else {
+      if (blacklist === undefined) {
+        blacklist = [];
+      }
+      blacklist.push(user.id);
+      fs.writeJson('blacklist.json', blacklist, (err) => {
+        if (err) console.log(err);
+        else msg.channel.sendMessage(`You have blacklisted the user: ${user.username}#${user.discriminator}.`);
+    });
+  }
+  });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 10,
+  botPerms: [],
+  requiredFuncs: []
+};
+
+exports.help = {
+  name: "blacklist",
+  description: "Blocks a user from using your bots commands.",
+  usage: "<user:user>",
+  usageDelim: ""
+};
