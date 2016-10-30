8cc332358a73b304a84bf28f1463a6b310436ae5
CyberiumShadow <CyberiumShadow@users.noreply.github.com>
2016-10-30 16:17:17 +1100

    Updated user check command to use server confs
    Also removed the obsolete prefix comamnd.

--- commands/Moderation/check.js
+++ commands/Moderation/check.js
@@ -1,11 +1,14 @@
 exports.run = (client, msg) => {
+	let keycheck = client.funcs.confs.hasKey('minAccAge');
+	if (!keycheck) {client.funcs.confs.addKey('minAccAge', 1800000)};
+	let accAge = msg.guildConf.minAccAge;
 	let mtime = msg.createdTimestamp;
-	let check = msg.guild.members.filter( m => (mtime - m.user.createdTimestamp) <= 1800000);
+	let check = msg.guild.members.filter( m => (mtime - m.user.createdTimestamp) <= accAge);
 	if (check.size > 0) {
 	let result = check.map(u=> `${u.user.username}#${u.user.discriminator}, Created:${((mtime - u.user.createdTimestamp)/1000/60).toFixed(0)} min(s) ago`).join('\n');
-	msg.reply(`The following users are less then 30 mins old\n \`\`\`\n ${result}\n\`\`\` `)
+	msg.reply(`The following users are less then the Minimum Account Age: \n \`\`\`xl\n${result}\n\`\`\` `)
 	} else {
-		msg.reply('No users less then 30 mins old were found')
+		msg.reply('No users less then Minimum Account Age were found in the guild.')
 	}
 };
 
