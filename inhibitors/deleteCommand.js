c596636dd05c6815af5b53c0c3486ca71f9aaca8
Faith <tba28012@hotmail.com>
2016-11-08 17:46:57 -0500

    Updated code

--- inhibitors/deleteCommand.js
+++ inhibitors/deleteCommand.js
@@ -2,18 +2,17 @@ exports.conf = {
   enabled: true,
   spamProtection: false
 };
+exports.init = (client) => {
+  if (!client.funcs.confs.hasKey("deleteCommand")) {
+    client.funcs.confs.addKey("deleteCommand", false);
+  }
+}
 
 exports.run = (client, msg, cmd) => {
   return new Promise((resolve, reject) => {
-    let conf = msg.guildConf;
-    if (!conf.delete) {
-      client.funcs.confs.addKey("deleteCommand", false);
-      conf = msg.guildConf;
-    }
-    if (!cmd.conf.ignoreDelete && conf.deleteCommand === true) {
+    if (!cmd.conf.ignoreDelete && msg.guildConf.deleteCommand === true) {
       msg.delete();
-      resolve();
-    } else {
+    }
       resolve();
     }
   });
