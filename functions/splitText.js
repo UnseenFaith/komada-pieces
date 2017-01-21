aab3b2fb130d21beae609ae7bc3b39cc1c89ee55
kyraNET <kyradiscord@gmail.com>
2017-01-21 21:38:04 +0100

    Fixed a bug

--- functions/splitText.js
+++ functions/splitText.js
@@ -1,4 +1,5 @@
 module.exports = (str, l) => {
-  var pos = str.substring(0, l).lastIndexOf(' ');
+  let x = str.substring(0, l).lastIndexOf(" ");
+  let pos = x === -1 ? l : x;
   return str.substring(0, pos);
 };
