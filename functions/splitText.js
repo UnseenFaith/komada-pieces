4d1b25519be0849f7693a97dda1d75a7383f67ab
CyberiumShadow <CyberiumShadow@users.noreply.github.com>
2017-01-22 21:27:46 +1100

    ESLinting of Pieces and Metadata.

--- functions/splitText.js
+++ functions/splitText.js
@@ -1,5 +1,5 @@
 module.exports = (str, l) => {
-  let x = str.substring(0, l).lastIndexOf(" ");
-  let pos = x === -1 ? l : x;
+  const x = str.substring(0, l).lastIndexOf(" ");
+  const pos = x === -1 ? l : x;
   return str.substring(0, pos);
 };
