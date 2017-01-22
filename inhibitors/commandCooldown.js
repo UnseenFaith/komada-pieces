a4069ff0d321bb3926a0d4e06f8d8b30e4d615fa
Verschaete B <BVerschaete@users.noreply.github.com>
2017-01-22 09:59:19 +0100

    commandCooldown inhibitor (#38)
    * added commandCooldown inhibitor
    
    * Removed my initials and put commandCooldown on disabled
    
    * Update commandSlowMode.js

---
+++ inhibitors/commandCooldown.js
@@ -0,0 +1,66 @@
+// Add this to the export.conf of any command to implement a cooldown per command
+//
+// cooldowns: {
+//     //Is the cooldown per player or guild
+//     scope: "guild",
+//     time: 15000
+// }
+
+const cooldowns = new Map();
+
+exports.conf = {
+    enabled: false,
+    spamProtection: true
+};
+
+exports.run = (client, msg, cmd) => {
+    return new Promise((resolve, reject) => {
+        const commandName = cmd.help.name;
+
+        //default behaviour
+        //change this to msg.channel.id or msg.guild.id to override default behaviour
+        let id = msg.author.id;
+
+        const standardCooldown = 1000;
+        let commandCooldown = standardCooldown;
+
+        //Override default behaviour if command.conf has cooldown variable
+        if(cmd.conf.cooldown) {
+            if(cmd.conf.cooldown.scope == "guild") {
+                id = msg.guild.id;
+            }
+
+            commandCooldown = cmd.conf.cooldown.time;
+        }
+        let entry = false;
+
+        if (!cooldowns.get(id)) cooldowns.set(id, {});
+
+        if(!cooldowns.get(id)[commandName]) cooldowns.get(id)[commandName] = createTimeOut();
+        else entry = true;
+
+        if (entry && commandCooldown != standardCooldown) {
+            let message = 'Please wait ' + commandCooldown/1000 + ' second(s) between !' +
+                commandName + ' commands.';
+
+            if(id == msg.guild.id) message += '\nThe cooldown for this command is shared across the guild.';
+
+            msg.channel.sendCode('asciidoc', message);
+        }
+
+        function createTimeOut() {
+            return setTimeout(() => {
+                let userCooldownBooleans = cooldowns.get(id);
+                delete userCooldownBooleans[commandName];
+
+                //delete key in map when its value is empty, just to keep the map clear
+                if (Object.keys(userCooldownBooleans).length === 0) {
+                    cooldowns.delete(id);
+                }
+            }, commandCooldown);
+        }
+
+        if (entry) reject();
+        else resolve();
+    });
+};
