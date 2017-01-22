256ab90fedd562d3fe865c5742b4a07f04ff8c02
CyberiumShadow <CyberiumShadow@users.noreply.github.com>
2017-01-22 21:27:46 +1100

    ESLinting of Pieces and Metadata.

--- inhibitors/commandSlowMode.js
+++ inhibitors/commandSlowMode.js
@@ -4,23 +4,20 @@ const ratelimit = 1250;
 
 exports.conf = {
   enabled: true,
-  spamProtection: true
+  spamProtection: true,
 };
 
-exports.run = (client, msg, cmd) => {
-  return new Promise((resolve, reject) => {
+exports.run = (client, msg, cmd) => new Promise((resolve, reject) => {
     // also available: msg.server.id , msg.channel.id
-    let slowmode_level = msg.author.id;
-    let entry = slowmode.get(slowmode_level);
-    if (!entry)
-      slowmode.set(slowmode_level, true);
-    if (timers[slowmode_level]) clearTimeout(timers[slowmode_level]);
-    timers[slowmode_level] = setTimeout(() => {
-      slowmode.delete(slowmode_level);
-      delete timers[slowmode_level];
-    }, ratelimit);
+  const slowmode_level = msg.author.id;
+  const entry = slowmode.get(slowmode_level);
+  if (!entry) { slowmode.set(slowmode_level, true); }
+  if (timers[slowmode_level]) clearTimeout(timers[slowmode_level]);
+  timers[slowmode_level] = setTimeout(() => {
+    slowmode.delete(slowmode_level);
+    delete timers[slowmode_level];
+  }, ratelimit);
 
-    if (entry) reject();
-    else resolve();
-  });
-};
+  if (entry) reject();
+  else resolve();
+});
