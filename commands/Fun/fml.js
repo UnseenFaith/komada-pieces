720b3cda11c6f4a56b56e64a57313873ad8a701a
York <YorkAARGH@users.noreply.github.com>
2017-02-15 20:22:04 +0000

    Update fml.js
    fmylife.com updated their website layout, update reflects that change.

--- commands/Fun/fml.js
+++ commands/Fun/fml.js
@@ -7,8 +7,8 @@ exports.run = (client, msg) => {
     if (err) return msg.reply(err);
     client.funcs.log(err.stack, "error");
     const root = HTMLParser.parse(res.text);
-    const article = root.querySelector(".post.article .fmllink");
-    return msg.channel.sendMessage(article.childNodes[0].text);
+    const article = root.querySelector('.block a');
+    return msg.channel.sendMessage(article.text);
   });
 };
 
