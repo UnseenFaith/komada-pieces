37b80410971f5250f0d1025efb2793c1e0507d35
kyraNET <kyranet@users.noreply.github.com>
2017-04-05 17:51:39 +0200

    Update check.js

--- commands/Moderation/check.js
+++ commands/Moderation/check.js
@@ -4,11 +4,11 @@ exports.run = (client, msg) => {
   const accAge = msg.guildConf.minAccAge;
   const mtime = msg.createdTimestamp;
   const check = msg.guild.members.filter(m => (mtime - m.user.createdTimestamp) <= accAge);
-  if (check.size > 0) {
+  if (check.size) {
     const result = check.map(u => `${u.user.username}#${u.user.discriminator}, Created:${((mtime - u.user.createdTimestamp) / 1000 / 60).toFixed(0)} min(s) ago`).join("\n");
-    msg.reply(`The following users are less then the Minimum Account Age: \n \`\`\`xl\n${result}\n\`\`\` `);
+    msg.reply(`The following users are less than the Minimum Account Age: \n \`\`\`xl\n${result}\n\`\`\` `);
   } else {
-    msg.reply("No users less then Minimum Account Age were found in the guild.");
+    msg.reply("No users less than Minimum Account Age were found in the guild.");
   }
 };
 
