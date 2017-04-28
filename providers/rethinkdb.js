52b1ea044461519c0dd2b357d5ae3b564ace2cda
kyraNET <kyranet@users.noreply.github.com>
2017-04-28 11:14:29 +0200

    Exportstify using current SQLite provider as template.

--- providers/rethinkdb.js
+++ providers/rethinkdb.js
@@ -5,91 +5,33 @@ const config = {
   db: "Komada",
 };
 
-class RethinkDB {
-  constructor() {
-    this.hasTable = RethinkDB.hasTable;
-    this.sync = RethinkDB.sync;
-    this.createTable = RethinkDB.createTable;
-    this.deleteTable = RethinkDB.deleteTable;
-
-    this.all = RethinkDB.all;
-    this.get = RethinkDB.get;
-    this.getRandom = RethinkDB.getRandom;
-    this.add = RethinkDB.add;
-    this.update = RethinkDB.update;
-    this.replace = RethinkDB.replace;
-    this.delete = RethinkDB.delete;
-  }
-
   /* Table methods */
-  static hasTable(table) {
-    return r.tableList().run()
-      .then(data => data.includes(table));
-  }
+exports.hasTable = table => r.tableList().run().then(data => data.includes(table));
 
-  static sync(table) {
-    return RethinkDB.hasTable(table).then((data) => {
-      if (!data) throw new Error("This table does not exist.");
-      return r.sync(table).run();
-    });
-  }
+exports.sync = table => r.table(table).sync().run();
 
-  static createTable(table) {
-    return RethinkDB.hasTable(table).then((data) => {
-      if (data) throw new Error("This table already exists.");
-      return r.tableCreate(table).run();
-    });
-  }
+exports.createTable = table => r.tableCreate(table).run();
 
-  static deleteTable(table) {
-    return RethinkDB.hasTable(table).then((data) => {
-      if (!data) throw new Error("This table does not exist.");
-      return r.tableDrop(table).run();
-    });
-  }
+exports.deleteTable = table => r.tableDrop(table).run();
 
   /* Document methods */
-  static all(table) {
-    return r.table(table) || null;
-  }
+exports.all = table => r.table(table) || null;
 
-  static get(table, id) {
-    return r.table(table).get(id) || null;
-  }
+exports.get = (table, id) => r.table(table).get(id) || null;
 
-  static getRandom(table) {
-    return RethinkDB.all(table).then(data => data[Math.floor(Math.random() * data.length)]);
-  }
+exports.getRandom = table => this.all(table).then(data => data[Math.floor(Math.random() * data.length)]);
 
-  static add(table, doc) {
-    if (!(doc instanceof Object)) throw new Error("Invalid input");
-    return r.table(table).insert(doc).run();
-  }
+exports.add = (table, doc) => r.table(table).insert(doc).run();
 
-  static update(table, id, doc) {
-    if (!(doc instanceof Object)) throw new Error("Invalid input");
-    return RethinkDB.get(table, id).then((data) => {
-      if (!data) throw new Error("Document not found.");
-      return data.update(doc).run();
-    });
-  }
+exports.update = (table, id, doc) => r.table(table).get(id).update(doc).run();
 
-  static replace(table, id, doc) {
-    if (!(doc instanceof Object)) throw new Error("Invalid input");
-    return RethinkDB.get(table, id).then((data) => {
-      if (!data) throw new Error("Document not found.");
-      return data.replace(doc).run();
-    });
-  }
+exports.append = (table, id, appendArray, doc) => r.table(table).get(id).update(object => ({ [appendArray]: object(appendArray).default([]).append(doc) })).run();
 
-  static delete(table, id, doc) {
-    if (!(doc instanceof Object)) throw new Error("Invalid input");
-    return RethinkDB.get(table, id).then((data) => {
-      if (!data) throw new Error("Document not found.");
-      return data.delete().run();
-    });
-  }
-}
+exports.replace = (table, id, doc) => r.table(table).get(id).replace(doc).run();
+
+exports.delete = (table, id) => r.table(table).get(id).delete().run();
+
+  /* Exports for the Download command */
 
 exports.help = {};
 exports.help.name = "rethinkdb";
@@ -98,8 +40,6 @@ exports.help.description = "Allows you use rethinkDB functionality throughout Ko
 exports.conf = config;
 exports.conf.requiredModules = ["rethinkdbdash"];
 
-/* eslint-disable global-require */
-exports.init = (client) => {
+exports.init = () => {
   r = require("rethinkdbdash")(config.db);
-  client.rethinkdb = RethinkDB;
 };
