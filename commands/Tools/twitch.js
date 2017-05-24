76303be27ac6ddbda7672cfefcbf3cba61c59dfc
Magnaboy <private123123a@hotmail.com>
2017-05-24 22:28:09 +1000

    Use deconstructor, and remove 'Fetching' message

--- commands/Tools/twitch.js
+++ commands/Tools/twitch.js
@@ -5,18 +5,17 @@ exports.run = async (client, msg, [twitchName]) => {
   const snekfetch = require("snekfetch");
   const moment = require("moment");
   try {
-    const res = await snekfetch.get(`https://api.twitch.tv/kraken/channels/${twitchName}?client_id=${clientID}`);
-    const creationDate = moment(res.body.created_at).format("DD-MM-YYYY");
+    const { body } = await snekfetch.get(`https://api.twitch.tv/kraken/channels/${twitchName}?client_id=${clientID}`);
+    const creationDate = moment(body.created_at).format("DD-MM-YYYY");
     const embed = new client.methods.Embed()
       .setColor(6570406)
-      .setThumbnail(res.body.logo)
-      .setAuthor(res.body.display_name, "https://i.imgur.com/OQwQ8z0.jpg", res.body.url)
-      .addField("Account ID", res.body._id, true)
-      .addField("Followers", res.body.followers, true)
+      .setThumbnail(body.logo)
+      .setAuthor(body.display_name, "https://i.imgur.com/OQwQ8z0.jpg", body.url)
+      .addField("Account ID", body._id, true)
+      .addField("Followers", body.followers, true)
       .addField("Created On", creationDate, true)
-      .addField("Channel Views", res.body.views, true);
-    const reply = await msg.channel.send(`Fetching info for ${twitchName}...`);
-    await reply.edit({ embed });
+      .addField("Channel Views", body.views, true);
+    await msg.channel.send({embed});
   } catch (e) {
     msg.reply("Unable to find account. Did you spell it correctly?");
     console.log(`Error in twitch.js: ${e}`);
