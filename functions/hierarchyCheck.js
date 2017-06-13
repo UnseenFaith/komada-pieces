950bffbed078edc9fe9d7371e9d3ba897c64621f
Michael Cumbers <me@michaelcumbers.ca>
2017-06-13 13:01:30 -0700

    async/await

--- functions/hierarchyCheck.js
+++ functions/hierarchyCheck.js
@@ -2,7 +2,8 @@
 Check if user is higher in guild's role hierarchy
 ...Basically an easy way to stop mods from performing actions on admins, etc
 
-    Usage: 
+    Usage:
+        - client scope
         - executor is the user object of the user executing the moderation command
         - target is the user object of the user the moderation command is affecting
         - guild is the guild object of the guild the command is being executed in
@@ -10,44 +11,38 @@ Check if user is higher in guild's role hierarchy
     Returns:
         - boolean true if executor is higher
         - boolean false if executor is lower or equal
+        - boolean false if either user isn't in the guild
 
 **/
 
-const func = function(executor, target, guild = null) {
-    return new Promise((resolve, reject) => {
+const func = async (client, executor, target, guild = null) => {
+    try{
         if (guild) {
-            guild.fetchMember(executor)
-            .then((executorGM) => {
-                guild.fetchMember(target)
-                .then((targetGM) => {
-                    
-                    if(executorGM.highestRole.position > targetGM.highestRole.position){
-                        resolve(true);
-                    } else {
-                        resolve(false);
-                    }
+            let executorMember = await guild.fetchMember(executor);
+            let targetMember = await guild.fetchMember(target);
+            
+            if(executorMember.highestRole.position > targetMember.highestRole.position){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+    }catch(err){
+        client.emit("log", err, "error");
+        client.emit("log", "Failed to fetch guildMember(s). Probably at least one isn't in this guild?", "error");
+        return false;
+    }
 
-                    })
-                .catch((err) => {
-                    client.emit("log", err, "error");
-                });
-            })
-            .catch((err) => {
-                client.emit("log", err, "error");
-            });
 
-        } else {
-            resolve(false);
-        }
-    });
+}
 
-};
+
 
 func.conf = { requiredModules: [] };
 func.help = {
-	name: "hierarchyCheck",
-	type: "functions",
-	description: "Checks to see that command executor is higher on guild's hierarchy than command target.",
+    name: "hierarchyCheck",
+    type: "functions",
+    description: "Checks to see that command executor is higher on guild's hierarchy than command target.",
 };
 
 module.exports = func;
