db4a9bbccf62b04e35613734e3f26217bd255172
Michael Cumbers <me@michaelcumbers.ca>
2017-06-18 13:19:25 -0700

    fetch user objects before fetching guildMember objects

--- functions/hierarchyCheck.js
+++ functions/hierarchyCheck.js
@@ -3,22 +3,24 @@ Check if user is higher in guild's role hierarchy
 ...Basically an easy way to stop mods from performing actions on admins, etc
 
     Usage:
+        - client scope
         - executor is the user object of the user executing the moderation command
         - target is the user object of the user the moderation command is affecting
         - guild is the guild object of the guild the command is being executed in
     Returns:
         - boolean true if executor is higher
         - boolean false if executor is lower or equal
-        - boolean false if either user isn't in the guild
+        - boolean false if no guild
 **/
 
-const func = async (executor, target, guild = null) => {
+const func = async (client, executor, target, guild = null) => {
   if (guild) {
-    const executorMember = await guild.fetchMember(executor);
-    const targetMember = await guild.fetchMember(target);
+    const executorMember = await guild.fetchMember(await client.fetchUser(executor.id));
+    const targetMember = await guild.fetchMember(await client.fetchUser(target.id));
 
     return executorMember.highestRole.position > targetMember.highestRole.position;
   }
+
   return false;
 };
 
