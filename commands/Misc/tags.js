2a7ec7c6b824d10030c43d8c32be5df7e1c51bad
kyraNET <kyradiscord@gmail.com>
2017-06-26 21:11:30 +0200

    More updates!
    Tags should be working now.

--- commands/Misc/tags.js
+++ commands/Misc/tags.js
@@ -1,27 +1,39 @@
-let db;
+exports.providerEngine = "json";
+
+exports.init = async (client) => {
+  if (client.providers.has(this.providerEngine)) this.provider = client.providers.get(this.providerEngine);
+  if (!(await this.provider.hasTable("tags"))) await this.provider.createTable("tags");
+};
 
 exports.run = async (client, msg, [action, ...contents]) => {
-  if (!client.providers.has("sqlite")) return msg.reply("this command requires the `sqlite` module which is not present.");
-  if (!contents && ["add", "delete"].includes(action)) return msg.reply("you must provide a name for this action.");
-  if (!action) {
-    const rows = await db.getAll(client, "tags");
-    return msg.channel.send(`**List of tags**: \`\`\`${rows.map(r => r.name).join(" | ")}\`\`\``);
-  } else if (action === "add") {
-    await db.insert(client, "tags", ["name", "count", "contents"], [contents[0], 0, contents.slice(1).join(" ")]);
-    return msg.reply(`The tag \`${contents[0]}\` has been added.`);
-  } else if (action === "delete") {
-    const row = await db.get(client, "tags", "name", contents[0]);
-    if (!row) return msg.reply("this tag doesn't seem to exist.");
-    await db.delete(client, "tags", row.id).catch(e => msg.reply(`I wasn't able to delete the tag because of the following reason: \n${e}`));
-    return msg.reply("the tag has been deleted. It's gone. For real, it no longer exists. It's pushing up the daisies.");
-  } else if (action === "random") {
-    const row = await db.getRandom(client, "tags");
-    await db.update(client, "tags", ["count"], [row.count++], "id", row.id);
-    return msg.channel.send(row.contents);
+  contents = contents[0] ? contents.join(" ") : null;
+  switch (action) {
+    case "add": {
+      await this.provider.insert("tags", contents.split(" ")[0], { count: 0, contents: contents.split(" ").slice(1).join(" ") });
+      return msg.reply(`The tag \`${contents[0]}\` has been added.`);
+    }
+    case "delete": {
+      const row = await this.provider.get("tags", contents);
+      if (!row) return msg.reply("this tag doesn't seem to exist.");
+      await this.provider.delete("tags", row.id).catch(e => msg.reply(`I wasn't able to delete the tag because of the following reason: \n${e}`));
+      return msg.reply("the tag has been deleted. It's gone. For real, it no longer exists. It's pushing up the daisies.");
+    }
+    case "random": {
+      const row = await this.provider.getRandom("tags");
+      await this.provider.update("tags", row.id, { count: row.count++ });
+      return msg.channel.send(row.contents);
+    }
+    default: {
+      if (contents) {
+        const rows = await this.provider.getAll("tags");
+        return msg.channel.send(`**List of tags**: \`\`\`${rows.map(r => r.name).join(" | ")}\`\`\``);
+      }
+      const row = await this.provider.get("tags", contents);
+      if (!row) return msg.reply("tag name not found.");
+      await this.provider.update("tags", row.id, { count: row.count++ });
+      return msg.channel.send(row.contents);
+    }
   }
-  const row = await db.get(client, "tags", "name", action).catch(() => msg.reply("tag name not found."));
-  await db.update(client, "tags", ["count"], [row.count++], "id", row.id);
-  return msg.channel.send(row.contents);
 };
 
 exports.conf = {
@@ -41,15 +53,4 @@ exports.help = {
   usage: "[add|delete|random] [contents:string] [...]",
   usageDelim: " ",
   type: "commands",
-};
-
-exports.init = async (client) => {
-  if (!client.providers.has("sqlite")) client.funcs.log("tag Command: No Database Found", "error");
-  db = client.providers.get("sqlite");
-  const res = await db.hasTable(client, "tags");
-  if (!res) {
-    const keys = "<name:str> <count:int> <contents:str> <enabled:bool> <embed:bool> <title:str> <footer:str>";
-    db.createTable(client, "tags", keys).catch(e => client.funcs.log(e, "error"));
-  }
-  client.config.init.push("tags");
 };
