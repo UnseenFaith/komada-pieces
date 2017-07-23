4a3362d4b202dfa088ccf7736311544abd3f7aed
Ryuuki <ryuukibeat@gmail.com>
2017-07-23 20:36:32 +0700

    fix purge?

--- commands/System/purge.js
+++ commands/System/purge.js
@@ -1,17 +1,17 @@
-exports.run = async (client, msg) => {
-  const user = msg.mentions.users.first();
-  const amount = parseInt(msg.content.split(" ").pop());
+exports.run = async (client, msg, [user, amount]) => {
+  user = msg.mentions.users.first();
+  amount = parseInt(msg.content.split(" ").pop());
 
   if (!amount) return msg.reply("Must specify an amount to delete!");
   if (!amount && !user) return msg.reply("Must specify a user and amount, or just an amount, of messages to purge!");
 
   if (user) {
-    msg.channel.fetchMessages({ limit: 100 }).then((messages) => {
+    return msg.channel.fetchMessages({ limit: 100 }).then((messages) => {
       const userMsgs = messages.filter(m => m.author.id === user.id).array().slice(0, amount);
       return msg.channel.bulkDelete(userMsgs).catch(error => console.log(error.stack));
     });
-  } else return msg.channel.bulkDelete(amount);
-  return console.log("Deleted!");
+  }
+  return msg.channel.bulkDelete(amount);
 };
 
 exports.conf = {
