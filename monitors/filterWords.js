fffc463785ee557025b124b526eb1c3262e79f91
Skillz4Killz <admin@ezlgg.com>
2017-08-01 21:59:01 -0400

    fixed a infinite loop error

--- monitors/filterWords.js
+++ monitors/filterWords.js
@@ -7,6 +7,8 @@ exports.conf = {
 };
 
 exports.run = (client, msg) => {
+  // Do not delete messages from the Bot itself otherwise it will run on a loop when it sends msgs in a DM to author or reports to a channel.
+  if (msg.author.bot) return;
   // Add your list of words you want to filter. This is a very powerful filter so be careful of using. For example, the word 'assign' will not be allowed if the word `ass` is present in the swear words. In other filters, they are easily breakable by typing 'ass.' but this filter will delete these types of breaks.
   const swearWords = ["damn", "shucks", "frak", "ass"];
 
