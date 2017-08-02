3e2d81553377052ea6645a1760811adffd1fa150
Skillz4Killz <admin@ezlgg.com>
2017-08-01 21:17:12 -0400

    fixed a couple of errors

--- monitors/filterWords.js
+++ monitors/filterWords.js
@@ -10,18 +10,18 @@ exports.run = (client, msg) => {
   // Add your list of words you want to filter. This is a very powerful filter so be careful of using. For example, the word 'assign' will not be allowed if the word `ass` is present in the swear words. In other filters, they are easily breakable by typing 'ass.' but this filter will delete these types of breaks.
   const swearWords = ["damn", "shucks", "frak", "ass"];
 
-  let badWords = swearWords.filter(word => message.content.toLowerCase().includes(word));
+  let badWords = swearWords.filter(word => msg.content.toLowerCase().includes(word));
   if(badWords.length > 0) {
     let badWordList = [];
     for (let i = 0; i < badWords.length; i++) {
       badWordList += badWords[i];
     }
     // DM the user regarding what words caused the message to be deleted.
-    message.author.send(`I deleted the message below because you used a word that is not allowed. : **${badWordList}**`);
+    msg.author.send(`I deleted the message below because you used a word that is not allowed. : **${badWordList}**`);
     // DM the user with the message incase they wish to copy paste -> edit and resend. Imagine what happens when by accident a user gets the message deleted for a mistake.
-    message.author.send(`${message.content}`);
+    msg.author.send(`${msg.content}`);
     // Delete a message
-    message.delete()
+    msg.delete()
 		  // Send a report of the effect to a channel of your choosing.
       // Please insert your channel id here for the channel you wish to recieve them in.
       .then((msg) => client.channels.get('CHANNELIDHERE').send(`Deleted message from ${msg.author} that said the \n\n **Naughty Words:** ${badWordList} in the server ${msg.guild} in the channel ${msg.channel}. \n\n **Full Message:** ${msg.content}`))
