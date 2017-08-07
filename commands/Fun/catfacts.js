2cd0a24d998668d21e38e018c37a125b1bb20533
Jack <jacknagle22@gmail.com>
2017-08-07 20:58:27 +1000

    Codacy pls

--- commands/Fun/catfacts.js
+++ commands/Fun/catfacts.js
@@ -1,6 +1,7 @@
 const snekfetch = require("snekfetch");
+
 exports.run = async (client, msg) => {
-    snekfetch.get("https://catfact.ninja/fact").then(res => {
+    snekfetch.get("https://catfact.ninja/fact").then((res) => {
         return msg.channel.send(`📢 **Catfact:** *${res.body.fact}*`);
     });
 
