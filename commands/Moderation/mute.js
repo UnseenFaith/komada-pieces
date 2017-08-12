bed0d6f447672901ce8f87f655d4d7881d0186f5
Skillz4Killz <admin@ezlgg.com>
2017-08-12 16:40:49 -0400

    converted vars to config system and used ternary operator on mute and role commands

--- commands/Moderation/mute.js
+++ commands/Moderation/mute.js
@@ -1,49 +1,38 @@
+exports.init = async (client) => {
+  const schema = client.settings.guilds.schema;
+  if (!schema.muteRoleName) {
+    await client.settings.guilds.add("muteRoleName", options);
+  }
+  if (!schema.muteRoleId) {
+    await client.settings.guilds.add("muteRoleId", options);
+  }
+  if (!schema.titleURL) {
+    await client.settings.guilds.add("titleURL", options);
+  }
+  if (!schema.reportChannelId) {
+    await client.settings.guilds.add("reportChannelId", options);
+  }
+}
+
 exports.run = async (client, msg, [user]) => {
   try {
-    // Please write the id number and name of the role you set up on your server for muting. To get a role id, you will need discord developer mode turned on. Note: Depending on how you set this up on your server the bot will mute on text/voice or both.
-    const muteRoleName = "ROLENAMEHERE";
-    const muteRoleId = "ROLEIDHERE";
-    // The URL you wish to have your users linked to when clicking on the title of the embed.
-    const titleURL = "URLHERE";
-    if (!msg.mentions.members.first().roles.get(muteRoleId)) {
-      msg.mentions.members.first().addRole(msg.guild.roles.find("name", muteRoleName));
-      const embed = new client.methods.Embed()
-        .setTitle("User Was Muted!")
-        .setAuthor(client.user.username, client.user.avatarURL)
-        /*
-        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-        */
-        .setColor(0x00AE86)
-        .setDescription(`${user} is now in the time-out corner. :smiley:`)
-        .setFooter("Made with Komada, Discord.js, & <3", client.user.avatarURL)
-        /*
-        * Takes a Date object, defaults to current date.
-        */
-        .setTimestamp()
-        .setURL(titleURL);
-      msg.reply({ embed });
-    } else {
-      msg.mentions.members.first().removeRole(msg.guild.roles.find("name", muteRoleName));
-      const embed = new client.methods.Embed()
-        .setTitle("User Is No Longer Muted!")
-        .setAuthor(client.user.username, client.user.avatarURL)
-        /*
-        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-        */
-        .setColor(0x00AE86)
-        .setDescription(`${user} is is no longer in the time-out corner. :smiley:`)
-        .setFooter("Made with Komada, Discord.js, & <3", client.user.avatarURL)
-        /*
-        * Takes a Date object, defaults to current date.
-        */
-        .setTimestamp()
-        .setURL(titleURL);
-      msg.reply({ embed });
-    }
+    const value = msg.mentions.members.first().roles.get(muteRoleId);
+    value ? msg.mentions.members.first().removeRole(msg.guild.roles.find("name", muteRoleName)) : msg.mentions.members.first().addRole(msg.guild.roles.find("name", muteRoleName));
+    const embed = new client.methods.Embed()
+      .setTitle(value ? "User Is No Longer Muted!" : "User Was Muted!")
+      .setAuthor(client.user.username, client.user.avatarURL)
+      // Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+      .setColor(0x00AE86)
+      .setDescription(value ? `${user} is is no longer in the time-out corner. :smiley:` : `${user} is now in the time-out corner. :smiley:`)
+      .setFooter("Made with Komada, Discord.js, & <3", client.user.avatarURL)
+      // Takes a Date object, defaults to current date.
+      .setTimestamp()
+      .setURL(titleURL);
+    msg.reply({ embed });
   } catch (e) {
     msg.reply("Some error occured with mute/un-muting the member. A report has been sent to the developers.");
     // The channel id to where the bot sends a error report when something goes wrong.
-    client.channels.get("CHANNELIDHERE").send(`There was an error trying to mute/un-mute: ${e} in ${msg.channel} on ${msg.guild} by ${msg.author}`);
+    client.channels.get(reportChannelId).send(`There was an error trying to mute/un-mute: ${e} in ${msg.channel} on ${msg.guild} by ${msg.author}`);
   }
 };
 
--- commands/Moderation/role.js
+++ commands/Moderation/role.js
@@ -1,57 +1,37 @@
+exports.init = async (client) => {
+  const schema = client.settings.guilds.schema;
+  if (!schema.titleURL) {
+    await client.settings.guilds.add("titleURL", options);
+  }
+  if (!schema.reportChannelId) {
+    await client.settings.guilds.add("reportChannelId", options);
+  }
+}
 exports.run = async (client, msg, [user, role]) => {
   try {
-    // The URL you wish to have your users linked to when clicking on the title of the embed.
-    const titleURL = "URLHERE";
-
+    const value = msg.mentions.members.first().roles.find("name", role);
     if (msg.guild.roles.exists("name", role)) {
-      if (msg.mentions.members.first().roles.find("name", role)) {
-        msg.mentions.members.first().removeRole(msg.guild.roles.find("name", role));
-        const embed = new client.methods.Embed()
-          .setTitle("Role Removed From User!")
-          .setAuthor(client.user.username, client.user.avatarURL)
-          /*
-          * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-          */
-          .setColor(0x00AE86)
-          .setDescription(`${user} has lost the ${role} role. :smiley:`)
-          .setFooter("Made with Komada, Discord.js, & <3", client.user.avatarURL)
-          /*
-          * Takes a Date object, defaults to current date.
-          */
-          .setTimestamp()
-          .setURL(titleURL);
-        msg.reply({ embed });
-      } else if (!msg.mentions.members.first().roles.find("name", role)) {
-        msg.mentions.members.first().addRole(msg.guild.roles.find("name", role));
-        const embed = new client.methods.Embed()
-          .setTitle("Role Added To User!")
-          .setAuthor(client.user.username, client.user.avatarURL)
-          /*
-          * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-          */
-          .setColor(0x00AE86)
-          .setDescription(`${user} has been given the ${role} role. :smiley:`)
-          .setFooter("Made with Komada, Discord.js, & <3", client.user.avatarURL)
-          /*
-          * Takes a Date object, defaults to current date.
-          */
-          .setTimestamp()
-          .setURL(titleURL);
-        msg.reply({ embed });
-      }
+      value ? msg.mentions.members.first().removeRole(msg.guild.roles.find("name", role)): msg.mentions.members.first().addRole(msg.guild.roles.find("name", role));
+      const embed = new client.methods.Embed()
+        .setTitle(value ? "Role Removed From User!" : "Role Added To User!")
+        .setAuthor(client.user.username, client.user.avatarURL)
+        // Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+        .setColor(0x00AE86)
+        .setDescription(value ? `${user} has lost the ${role} role. :smiley:` : `${user} has been given the ${role} role. :smiley:`)
+        .setFooter("Made with Komada, Discord.js, & <3", client.user.avatarURL)
+        // Takes a Date object, defaults to current date.
+        .setTimestamp()
+        .setURL(titleURL);
+      msg.reply({ embed });
     } else {
       const embed = new client.methods.Embed()
         .setTitle("Role Not Found On Server!")
         .setAuthor(client.user.username, client.user.avatarURL)
-        /*
-         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-         */
+        // Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
         .setColor(0x00AE86)
         .setDescription(`I am sorry but ${role} role does not exist. :cry:`)
         .setFooter("Made with Komada, Discord.js, & <3", client.user.avatarURL)
-        /*
-         * Takes a Date object, defaults to current date.
-         */
+        // Takes a Date object, defaults to current date.
         .setTimestamp()
         .setURL(titleURL);
       msg.reply({ embed });
@@ -59,7 +39,7 @@ exports.run = async (client, msg, [user, role]) => {
   } catch (e) {
     msg.reply("Some error occured with adding a role to the member. A report has been sent to the developers.");
     // Please insert the channel id to where you want to recieve the error reports.
-    client.channels.get("CHANNELIDHERE").send(`There was an error trying to add a role: ${e} in ${msg.channel} on ${msg.guild} by ${msg.author}`);
+    client.channels.get(reportChannelId).send(`There was an error trying to add a role: ${e} in ${msg.channel} on ${msg.guild} by ${msg.author}`);
   }
 };
 
