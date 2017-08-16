ef56dfa9c0a15137bd43bfb48c09b270194c181e
Skillz4Killz <admin@ezlgg.com>
2017-08-16 17:53:48 -0400

    fixed a couple errors

--- commands/Tools/contact.js
+++ commands/Tools/contact.js
@@ -2,41 +2,41 @@ exports.init = async (client) => {
   const schema = client.settings.guilds.schema;
   if (!schema.bugChannelId) {
     await client.settings.guilds.add("bugChannelId", {
-      "type":"String",
-      "default":null,
-      "array":false,
-      "sql":"TEXT NOT NULL DEFAULT ','"
+      type: "String",
+      default: null,
+      array: false,
+      sql: "TEXT NOT NULL DEFAULT ','",
     });
   }
   if (!schema.ideaChannelId) {
     await client.settings.guilds.add("ideaChannelId", {
-      "type":"String",
-      "default":null,
-      "array":false,
-      "sql":"TEXT NOT NULL DEFAULT ','"
+      type: "String",
+      default: null,
+      array: false,
+      sql: "TEXT NOT NULL DEFAULT ','",
     });
   }
   if (!schema.messageChannelId) {
     await client.settings.guilds.add("titleURL", {
-      "type":"String",
-      "default":null,
-      "array":false,
-      "sql":"TEXT NOT NULL DEFAULT ','"
+      type: "String",
+      default: null,
+      array: false,
+      sql: "TEXT NOT NULL DEFAULT ','",
     });
   }
   if (!schema.reportChannelId) {
     await client.settings.guilds.add("reportChannelId", {
-      "type":"String",
-      "default":null,
-      "array":false,
-      "sql":"TEXT NOT NULL DEFAULT ','"
+      type: "String",
+      default: null,
+      array: false,
+      sql: "TEXT NOT NULL DEFAULT ','",
     });
   }
 };
 
- exports.run = async (client, msg, [type, ...content]) => {
-   // Declares all necessary variables to use for the command.
-   const { bugChannelId, ideaChannelId, messageChannelId, reportChannelId } = msg.guild.settings;
+exports.run = async (client, msg, [type, ...content]) => {
+  // Declares all necessary variables to use for the command.
+  const { bugChannelId, ideaChannelId, messageChannelId, reportChannelId } = msg.guild.settings;
   try {
     // Determines which type of contact and sends to the appropriate channel.
     if (type === "bug") {
