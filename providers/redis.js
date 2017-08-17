3899e62eb0d4a5ef0542478d6651e1e3553a82f3
kyraNET <kyranet@users.noreply.github.com>
2017-08-17 23:41:33 +0200

    Create redis.js

---
+++ providers/redis.js
@@ -0,0 +1,87 @@
+const Redis = require("redis-nextra");
+
+const redis = new Redis({});
+
+redis
+  .on("connect", () => console.log("Redis Connected"))
+  .on("reconnect", () => console.warn("Redis is reconnecting"))
+  .on("error", err => console.error("Redis error:", err));
+
+/* Table methods */
+
+/**
+ * Checks if the table exists.
+ * @param {string} table the name of the table you want to check.
+ * @returns {boolean}
+ */
+exports.hasTable = table => redis.tables.has(table);
+
+/**
+ * Creates a new table.
+ * @param {string} table the name for the new table.
+ * @returns {Object}
+ */
+exports.createTable = table => redis.createTable(table);
+
+/**
+ * Deletes a table.
+ * @param {string} table the name of the table you want to drop.
+ * @returns {Object}
+ */
+exports.deleteTable = table => redis.deleteTable(table);
+
+/* Document methods */
+
+/**
+ * Get all entries from a table.
+ * @param {string} table the name of the table you want to get the data from.
+ * @returns {?array}
+ */
+exports.getAll = table => redis.table(table).keys("*");
+
+/**
+ * Get an entry from a table.
+ * @param {string} table the name of the table.
+ * @param {string|number} id the entry's ID.
+ * @returns {?Object}
+ */
+exports.get = (table, id) => redis.table(table).get(id);
+
+/**
+ * Check if an entry exists from a table.
+ * @param {string} table the name of the table.
+ * @param {string|number} id the entry's ID.
+ * @returns {boolean}
+ */
+exports.has = (table, id) => redis.table(table).has(id);
+
+/**
+ * Insert a new document into a table.
+ * @param {string} table the name of the table.
+ * @param {string} id the id of the record.
+ * @param {Object} value the object you want to insert in the table.
+ * @param {?number} timer Amount of time in milliseconds for the value to expirate.
+ * @returns {Object}
+ */
+exports.set = (table, id, value, timer) => redis.table(table).set(id, value, timer);
+
+/**
+ * Delete an entry from the table.
+ * @param {string} table the name of the table.
+ * @param {string|number} id the entry's ID.
+ * @returns {Object}
+ */
+exports.delete = (table, id) => redis.table(table).del(id);
+
+exports.conf = {
+  enabled: false,
+  cache: true,
+  moduleName: "redis",
+  requiredModules: ["redis-nextra"],
+};
+
+exports.help = {
+  name: "redis",
+  type: "providers",
+  description: "Allows you use Redis functionality throughout Komada.",
+};
