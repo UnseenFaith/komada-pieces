ef5d76301fe1ea099b1f45e6b54063dc9ee1ef7c
Skillz4Killz <gankstarsleague@gmail.com>
2017-09-15 16:55:19 -0400

    Update filterWords.js

--- monitors/filterWords.js
+++ monitors/filterWords.js
@@ -6,7 +6,7 @@ exports.run = async (client, msg) => {
   if (badWords.length > 0) {
     await msg.author.send(`I deleted the message below because you used a word that is not allowed. : **${badWords.join(", ")}**`);
     await msg.author.send(msg.content);
-    msg.delete().then(msg => msg.guild.channels.get(modLog).send(`Deleted message from ${msg.author.tag} that said the \n\n **Naughty Words:** ${badWords} in the server ${msg.guild.name} in the channel ${msg.channel.name}. \n\n **Full Message:** ${msg.content}`))
+    msg.delete().then(m => m.guild.channels.get(modLog).send(`Deleted message from ${m.author.tag} that said the \n\n **Naughty Words:** ${badWords} in the server ${m.guild.name} in the channel ${m.channel.name}. \n\n **Full Message:** ${m.content}`))
       .catch(console.error);
   }
 };
