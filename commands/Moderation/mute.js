d078c73697da779ae3f9c23b2af4408e8463ad74
MrJacz <jacknagle22@gmail.com>
2017-09-16 06:19:24 +1000

    Update mute.js

--- commands/Moderation/mute.js
+++ commands/Moderation/mute.js
@@ -1,5 +1,4 @@
-exports.run = async (client, msg, [user]) => {
-  const member = msg.guild.members.fetch(user).catch(e => { throw e; });
+exports.run = async (client, msg, [member]) => {
   const { muteRole } = msg.guild.settings;
   if (!muteRole) return msg.reply("Sorry you have not created a mute role for this server.");
   const value = member.roles.get(muteRole);
@@ -9,7 +8,7 @@ const role = msg.guild.roles.get(muteRole);
   } else {
     await member.addRole(role);
   }
-  return msg.send(`${user} is ${value ? "not any longer" : "now"} muted. 😄`);
+  return msg.send(`${member} is ${value ? "not any longer" : "now"} muted. 😄`);
 };
 
 exports.conf = {
@@ -25,7 +24,7 @@ exports.conf = {
 exports.help = {
   name: "mute",
   description: "Mutes/unmutes a person on both text and voice.",
-  usage: "<user:member>",
+  usage: "<member:member>",
   usageDelim: "",
   extendedHelp: "1) mute @user\n2) Requires the user to have the modRole as per your configurations\n3) Bot requires Manage Role permissions.\n4) Requires a role with the muteRole settings you made set up without any permissions and at a high level in the role settings as well as each channel permissions being edited with its settings.",
 };
