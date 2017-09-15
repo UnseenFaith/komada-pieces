e722ce1efbd1fbe45f3b590b578f457d111da1be
Skillz4Killz <gankstarsleague@gmail.com>
2017-09-15 15:06:03 -0400

    small fix to filterWord to fix christmas tree

--- monitors/filterWords.js
+++ monitors/filterWords.js
@@ -1,15 +1,14 @@
 exports.run = async (client, msg) => {
   if (!msg.guild) return;
   const modlog = msg.guild.settings.modLog;
-  if (msg.deletable && msg.guild.settings.swearWords.length > 0) {
-    const badWords = msg.guild.settings.swearWords.filter(word => msg.content.toLowerCase().includes(word));
-    if (badWords.length > 0) {
-      await msg.author.send(`I deleted the message below because you used a word that is not allowed. : **${badWords.join(", ")}**`);
-      await msg.author.send(msg.content);
-      await msg.delete()
-        .then(msg.guild.channels.get(modlog).send(`Deleted message from ${msg.author} that said the \n\n **Naughty Words:** ${badWords} in the server ${msg.guild} in the channel ${msg.channel}. \n\n **Full Message:** ${msg.content}`))
-        .catch(console.error);
-    }
+  if (!msg.deletable && !msg.guild.settings.swearWords.length > 0) return;
+  const badWords = msg.guild.settings.swearWords.filter(word => msg.content.toLowerCase().includes(word));
+  if (badWords.length > 0) {
+    await msg.author.send(`I deleted the message below because you used a word that is not allowed. : **${badWords.join(", ")}**`);
+    await msg.author.send(msg.content);
+    await msg.delete()
+      .then(msg.guild.channels.get(modlog).send(`Deleted message from ${msg.author} that said the \n\n **Naughty Words:** ${badWords} in the server ${msg.guild} in the channel ${msg.channel}. \n\n **Full Message:** ${msg.content}`))
+      .catch(console.error);
   }
 };
 
