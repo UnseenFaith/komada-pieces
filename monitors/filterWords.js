b67891a72a99ec1dbfb5159dd23bb5bb88ac594c
Skillz4Killz <gankstarsleague@gmail.com>
2017-09-28 13:39:05 -0400

    fixed filterwords

--- monitors/filterWords.js
+++ monitors/filterWords.js
@@ -3,12 +3,12 @@ exports.run = async (client, msg) => {
   const { modLog, swearWords } = msg.guild.settings;
   if (!msg.deletable && !swearWords.length > 0) return;
   const badWords = swearWords.filter(word => msg.content.toLowerCase().includes(word));
-  if (badWords.length > 0) {
-    await msg.author.send(`I deleted the message below because you used a word that is not allowed. : **${badWords.join(", ")}**`);
-    await msg.author.send(msg.content);
-    msg.delete().then(m => m.guild.channels.get(modLog).send(`Deleted message from ${m.author.tag} that said the \n\n **Naughty Words:** ${badWords} in the server ${m.guild.name} in the channel ${m.channel.name}. \n\n **Full Message:** ${m.content}`))
-      .catch(console.error);
-  }
+  if (!badWords.length > 0) return
+  await msg.author.send(`I deleted the message below because you used a word that is not allowed. : **${badWords.join(", ")}**`);
+  await msg.author.send(msg.content);
+  msg.delete();
+  if (!modLog) return
+  return msg.guild.channels.get(modLog).send(`Deleted message from ${msg.author.tag} that said the \n\n **Naughty Words:** ${badWords} in the server ${msg.guild.name} in the channel ${msg.channel.name}. \n\n **Full Message:** ${msg.content}`);
 };
 
 exports.conf = {
