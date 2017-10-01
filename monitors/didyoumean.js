34135c047ef3c52e09e22187b819058867229474
CyberiumShadow <CyberiumShadow@users.noreply.github.com>
2017-10-01 22:36:49 +1100

    Part 1 of Kyra's changes

--- monitors/didyoumean.js
+++ monitors/didyoumean.js
@@ -3,22 +3,20 @@
 const levenshtein = require("fast-levenshtein");
 
 exports.run = async (client, msg) => {
-  if (msg.author.bot) return;
-  if (client.config.selfbot && msg.author.id !== client.user.id) return;
+  if (msg.author.bot || (client.config.selfbot && msg.author.id !== client.user.id)) return;
 
   const { prefixLength, command, prefix } = parseCommand(client, msg);
   if (!prefixLength) return;
-  if (command.length && !(client.commands.has(command) || client.aliases.has(command))) {
-    const distances = [];
-    client.commands.filter(c => c.conf.permLevel <= msg.member.permLevel).forEach((val, cmd) => distances.push({
-      dist: levenshtein.get(cmd, command),
-      cmd,
-    }));
-    distances.sort((a, b) => (a.score < b.score ? 1 : -1));
-    if (distances[0] && distances[0].dist <= 1) {
-      const message = await msg.channel.send(`|\`❔\`| Did you mean \`${prefix + distances[0].cmd}\`?`);
-      if (message.deletable) message.delete({ timeout: 10000 });
-    }
+  if (!command.length && (client.commands.has(command) || client.aliases.has(command))) return;
+
+  const distances = [];
+  client.commands.filter(c => c.conf.permLevel <= msg.member.permLevel).forEach((val, cmd) => distances.push({
+    dist: levenshtein.get(cmd, command),
+    cmd,
+  }));
+  distances.sort((a, b) => (a.score < b.score ? 1 : -1));
+  if (distances[0] && distances[0].dist <= 1) {
+    await msg.send(`|\`❔\`| Did you mean \`${prefix + distances[0].cmd}\`?`);
   }
 };
 
