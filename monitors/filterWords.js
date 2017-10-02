b0b97e15bc2510fbd57c2f0752db867c58559c04
Skillz4Killz <gankstarsleague@gmail.com>
2017-10-01 20:19:25 -0400

    linter

--- monitors/filterWords.js
+++ monitors/filterWords.js
@@ -3,11 +3,11 @@ exports.run = async (client, msg) => {
   const { modLog, swearWords } = msg.guild.settings;
   if (!msg.deletable && !swearWords.length > 0) return;
   const badWords = swearWords.filter(word => msg.content.toLowerCase().includes(word));
-  if (!badWords.length > 0) return
+  if (!badWords.length > 0) return;
   await msg.author.send(`I deleted the message below because you used a word that is not allowed. : **${badWords.join(", ")}**`);
   await msg.author.send(msg.content);
   msg.delete();
-  if (!modLog) return
+  if (!modLog) return;
   return msg.guild.channels.get(modLog).send(`Deleted message from ${msg.author.tag} that said the \n\n **Naughty Words:** ${badWords} in the server ${msg.guild.name} in the channel ${msg.channel.name}. \n\n **Full Message:** ${msg.content}`);
 };
 
