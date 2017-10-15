fcfbffbb6c0fbf07ad2c53d515e80078ebe1b579
CyberiumShadow <CyberiumShadow@users.noreply.github.com>
2017-10-16 10:17:02 +1100

    Requested Changes made

--- monitors/nomention.js
+++ monitors/nomention.js
@@ -2,8 +2,8 @@ exports.run = (client, msg) => {
   if (!msg.guild || msg.author.id === client.user.id) return;
   const user = `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`;
   const channel = `#${msg.channel.name} (${msg.channel.id}) from ${msg.guild.name}`;
-  if (msg.mentions.everyone) client.console.log(`${user} mentioned everyone in ${channel}`);
-  else if (msg.mentions.users.has(client.user.id)) client.console.log(`${user} mentioned you in ${channel}`);
+  if (msg.mentions.everyone) client.emit("log", `${user} mentioned everyone in ${channel}`);
+  else if (msg.mentions.users.has(client.user.id)) client.emit("log", `${user} mentioned you in ${channel}`);
 };
 
 exports.conf = {
