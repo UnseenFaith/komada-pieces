f6718975dcaa21fa333f0b9112faa39b3ba9cf61
Link <whiteknight539@gmail.com>
2017-10-18 13:03:23 -0700

    Gave better api
    Previous api gave mp4 files quite often and added in catching errors.

--- commands/Fun/dog.js
+++ commands/Fun/dog.js
@@ -1,14 +1,14 @@
 const snek = require("snekfetch");
 
 exports.run = async (client, msg) => {
-  const { body } = await snek.get("https://random.dog/woof.json");
-  return msg.channel.send({ files: [{ attachment: body.url, name: `dog.${body.url.split(".")[2]}` }] });
+  const { body: data } = await snek.get("https://dog.ceo/api/breeds/image/random");
+  await msg.channel.send({ files: [{ attachment: data.message, name: "Doggy.png" }] }).catch(e => msg.channel.send(e));
 };
 
 exports.conf = {
   enabled: true,
   runIn: ["text", "dm", "group"],
-  aliases: ["dawg", "duwg", "woof", "bork", "bark", "duggo", "doggo", "dug"],
+  aliases: ["dawg", "duwg", "woof", "bork", "stitch", "bark", "duggo", "doggo", "dug"],
   permLevel: 0,
   botPerms: ["SEND_MESSAGES"],
   requiredFuncs: [],
