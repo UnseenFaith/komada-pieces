d4a81ada76dd0060f9b275740ee935c54cc8a453
Link <whiteknight539@gmail.com>
2017-10-17 19:41:16 -0700

    Cat command
    Because why shouldn't there be a cat image command

---
+++ commands/Fun/cat.js
@@ -0,0 +1,26 @@
+const snek = require("snekfetch");
+
+exports.run = async (client, msg) => {
+  try {
+    const { body } = await snek.get("http://random.cat/meow");
+    await msg.send({ files: [{ attachment: body.file, name: `cat.${body.file.split(".")[2]}` }] });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text", "dm", "group"],
+  permLevel: 0,
+  botPerms: ["SEND_MESSAGES"],
+  requiredFuncs: [],
+  requiredSettings: [],
+};
+
+exports.help = {
+  name: "cat",
+  description: "Gathers a cat image from random.cat website",
+  usage: "",
+  usageDelim: "",
+};
