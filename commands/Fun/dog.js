098bd8b28474884c8700ea086bff5129f326b102
Link <whiteknight539@gmail.com>
2017-10-17 19:32:27 -0700

    Dog command
    Because why shouldn't there be a dog command

---
+++ commands/Fun/dog.js
@@ -0,0 +1,23 @@
+const snek = require("snekfetch");
+
+exports.run = async (client, msg) => {
+  const { body } = await snek.get("https://random.dog/woof.json");
+  return msg.channel.send({ files: [{ attachment: body.url, name: `dog.${body.url.split(".")[2]}` }] });
+};
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text", "dm", "group"],
+  aliases: ["dawg", "duwg", "woof", "bork", "stitch", "bark", "duggo", "doggo", "dug"],
+  permLevel: 0,
+  botPerms: ["SEND_MESSAGES"],
+  requiredFuncs: [],
+  requiredSettings: [],
+};
+
+exports.help = {
+  name: "dog",
+  description: "Gathers a dog image from random.dog website",
+  usage: "",
+  usageDelim: "",
+};
