2d287b74a8fcd70ff2dee49c81ebee8129e3c950
gchippendale <gchippendale@Hotmail.com>
2018-02-11 00:53:10 +1100

    Change order of checks, include catch for DM

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,9 +1,9 @@
 const MIN_MEMBERS = 3; // Minimum amount of MEMBERS that must be in a guild.
 const BOT_RATIO = 70; // The percentage of Members that are Bots in a guild must be less than this.
 
-exports.run = (client, guild) => {
-  if (guild.members.filter(u => u.user.bot).size * 100 / guild.memberCount >= BOT_RATIO || guild.memberCount < MIN_MEMBERS) {
-    guild.owner.send(`I have left your server \`${guild.name}\` because it looks like a bot farm, or a server with only me and you.`);
+exports.run = async (client, guild) => {
+  if (guild.memberCount < MIN_MEMBERS || guild.members.filter(u => u.user.bot).size * 100 / guild.memberCount >= BOT_RATIO) {
+    guild.owner.send(`I have left your server \`${guild.name}\` because it looks like a bot farm, or a server with only me and you.`).catch(() => null);
     guild.leave();
   }
 };
